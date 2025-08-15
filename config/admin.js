82a21ddb40881a4a97aa009dd2484b92d59e8c35
qoec <54418018+qoec@users.noreply.github.com>
2025-08-15 22:41:55 +0300

    local machine fix

--- config/admin.js
+++ config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: 'AcfArniaZ3ojY0JGOgDzjA==',
   },
   transfer: {
     token: {
