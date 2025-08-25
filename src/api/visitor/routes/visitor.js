9adc0a8cafc64e147b81162031cf84175baecd47
qoec <54418018+qoec@users.noreply.github.com>
2025-08-25 21:59:23 +0300

    deleted useless entries

--- src/api/visitor/routes/visitor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * visitor router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::visitor.visitor');
--- src/api/visitor/services/visitor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * visitor service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::visitor.visitor');
